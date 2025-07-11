f08b63850f7f3cdcd5d38a5979983cb5a4066459
swirl <180882683+lolswirl@users.noreply.github.com>
2025-07-11 15:26:13 -0400

    add new styling to app bar

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,225 +1,255 @@
 import * as React from "react";
 import {
-  AppBar,
-  Box,
-  Toolbar,
-  IconButton,
-  Typography,
-  Menu,
-  MenuItem,
-  Container,
-  Button,
-  Divider,
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
+    AppBar,
+    Box,
+    Toolbar,
+    IconButton,
+    Typography,
+    Container,
+    Button,
+    Divider,
+    Drawer,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText,
+    SvgIcon,
 } from "@mui/material";
-import {
-  Menu as MenuIcon,
-  ArrowDropDown,
-  ArrowRight,
-  Brightness4,
-  Brightness7,
-  ArrowDropDown as ArrowDownIcon,
-} from "@mui/icons-material";
+import { Menu as MenuIcon } from "@mui/icons-material";
 import { useThemeContext } from "../../context/ThemeContext.tsx";
 import { GetTitle } from "../../util/stringManipulation.tsx";
-import { graphPages } from "../../pages/graphs/GraphPages.tsx";
 import { useSpec } from "../../context/SpecContext.tsx";
 import { useLocation } from "react-router-dom";
 import SpecDisplay from "../SpecializationSelect/SpecDisplay.tsx";
+import SpecializationSelect from "../SpecializationSelect/SpecializationSelect.tsx";
+
+const MoonIcon = () => (
+    <SvgIcon viewBox="0 0 16 16" sx={{ fontSize: 15 }}>
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M1.5 8.00005C1.5 5.53089 2.99198 3.40932 5.12349 2.48889C4.88136 3.19858 4.75 3.95936 4.75 4.7501C4.75 8.61609 7.88401 11.7501 11.75 11.7501C11.8995 11.7501 12.048 11.7454 12.1953 11.7361C11.0955 13.1164 9.40047 14.0001 7.5 14.0001C4.18629 14.0001 1.5 11.3138 1.5 8.00005ZM6.41706 0.577759C2.78784 1.1031 0 4.22536 0 8.00005C0 12.1422 3.35786 15.5001 7.5 15.5001C10.5798 15.5001 13.2244 13.6438 14.3792 10.9921L13.4588 9.9797C12.9218 10.155 12.3478 10.2501 11.75 10.2501C8.71243 10.2501 6.25 7.78767 6.25 4.7501C6.25 3.63431 6.58146 2.59823 7.15111 1.73217L6.41706 0.577759ZM13.25 1V1.75V2.75L14.25 2.75H15V4.25H14.25H13.25V5.25V6H11.75V5.25V4.25H10.75L10 4.25V2.75H10.75L11.75 2.75V1.75V1H13.25Z"
+            fill="currentColor"
+        />
+    </SvgIcon>
+);
+
+const SunIcon = () => (
+    <SvgIcon viewBox="0 0 16 16" sx={{ fontSize: 15 }}>
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M8.75 0.75V0H7.25V0.75V2V2.75H8.75V2V0.75ZM11.182 3.75732L11.7123 3.22699L12.0659 2.87344L12.5962 2.34311L13.6569 3.40377L13.1265 3.9341L12.773 4.28765L12.2426 4.81798L11.182 3.75732ZM8 10.5C9.38071 10.5 10.5 9.38071 10.5 8C10.5 6.61929 9.38071 5.5 8 5.5C6.61929 5.5 5.5 6.61929 5.5 8C5.5 9.38071 6.61929 10.5 8 10.5ZM8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12ZM13.25 7.25H14H15.25H16V8.75H15.25H14H13.25V7.25ZM0.75 7.25H0V8.75H0.75H2H2.75V7.25H2H0.75ZM2.87348 12.0659L2.34315 12.5962L3.40381 13.6569L3.93414 13.1265L4.28769 12.773L4.81802 12.2426L3.75736 11.182L3.22703 11.7123L2.87348 12.0659ZM3.75735 4.81798L3.22702 4.28765L2.87347 3.9341L2.34314 3.40377L3.4038 2.34311L3.93413 2.87344L4.28768 3.22699L4.81802 3.75732L3.75735 4.81798ZM12.0659 13.1265L12.5962 13.6569L13.6569 12.5962L13.1265 12.0659L12.773 11.7123L12.2426 11.182L11.182 12.2426L11.7123 12.773L12.0659 13.1265ZM8.75 13.25V14V15.25V16H7.25V15.25V14V13.25H8.75Z"
+            fill="currentColor"
+        />
+    </SvgIcon>
+);
 
 const pages = [
-  { label: "When do I ramp?", path: "/when-do-i-ramp" },
-  { label: "Spell Timeline", path: "/timeline" },
-  { label: "Graphs & Analysis Tools", path: "/graphs" },
+    { label: "Home", path: "/" },
+    { label: "When do I ramp?", path: "/when-do-i-ramp" },
+    { label: "Spell Timeline", path: "/timeline" },
+    { label: "Graphs & Analysis Tools", path: "/graphs" },
 ];
 
 function ResponsiveAppBar() {
-  const location = useLocation();
-  const [dropdownAnchor, setDropdownAnchor] = React.useState<null | HTMLElement>(null);
-  const [drawerOpen, setDrawerOpen] = React.useState(false);
-  const [mobileGraphsOpen, setMobileGraphsOpen] = React.useState(false);
-  const { toggleTheme, themeMode } = useThemeContext();
-  const { spec } = useSpec();
-  const hoverColor = themeMode === "dark" ? "#90caf9" : "#212121";
+    const location = useLocation();
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
+    const { toggleTheme, themeMode } = useThemeContext();
+    const { spec, setSpec } = useSpec();
+    const hoverColor = themeMode === "dark" ? "#90caf9" : "#212121";
 
-  const isGraphPage = graphPages.some(({ path }) => location.pathname === path);
+    const handleDrawerToggle = () => setDrawerOpen((prev) => !prev);
 
-  const handleDrawerToggle = () => setDrawerOpen((prev) => !prev);
-
-  const renderDropdownLinks = () =>
-    graphPages
-      .filter(({ label }) => label.toLowerCase() !== "graphs")
-      .map(({ label, path }) => (
-        <MenuItem
-          key={GetTitle(label)}
-          component="a"
-          href={path}
-          onClick={() => setDropdownAnchor(null)}
-          sx={{
-            color: location.pathname === path ? hoverColor : "inherit",
-            fontWeight: location.pathname === path ? 700 : 400,
-            "&:hover": { color: hoverColor },
-            textTransform: "none"
-          }}
+    // sidebar drawer for mobile
+    const drawer = (
+        <Box
+            sx={{ width: 250, display: "flex", flexDirection: "column", height: "100%" }}
+            role="presentation"
+            onClick={handleDrawerToggle}
+            onKeyDown={handleDrawerToggle}
         >
-          {GetTitle(label)}
-        </MenuItem>
-      ));
+            <Box sx={{ p: 2, display: "flex", alignItems: "center", gap: 1, whiteSpace: "nowrap" }}>
+                <Typography
+                    variant="h6"
+                    sx={{
+                        fontWeight: 700,
+                        flexGrow: 1,
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                    }}
+                >
+                    𖦹 {GetTitle("Wait, I'm Ramping!")}
+                </Typography>
+            </Box>
+            <Divider />
+            <List>
+                {pages.map(({ label, path }) => (
+                    <ListItem key={label} disablePadding>
+                        <ListItemButton component="a" href={path} selected={location.pathname === path}>
+                            <ListItemText primary={GetTitle(label)} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+                
+            </List>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ p: 2, display: "flex", alignItems: "center", gap: 1 }}>
+                <Box
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 1,
+                        p: 1,
+                        border: "1px solid rgba(255,255,255,0.2)",
+                        borderRadius: 2,
+                        bgcolor: "rgba(255,255,255,0.05)",
+                    }}
+                >
+                    {spec && <SpecializationSelect selectedSpec={spec} onSpecChange={() => {}} />}
+                    <IconButton onClick={toggleTheme} sx={{ color: "white" }}>
+                        {themeMode === "dark" ? <MoonIcon /> : <SunIcon />}
+                    </IconButton>
+                </Box>
+            </Box>
+        </Box>
+    );
 
-  // sidebar drawer for mobile
-  const drawer = (
-    <Box
-      sx={{ width: 250, display: "flex", flexDirection: "column", height: "100%" }}
-      role="presentation"
-      onClick={handleDrawerToggle}
-      onKeyDown={handleDrawerToggle}
-    >
-      <Box sx={{ p: 2, display: "flex", alignItems: "center", gap: 1, whiteSpace: "nowrap" }}>
-        <Typography
-          variant="h6"
-          sx={{
-            fontWeight: 700,
-            flexGrow: 1,
-            whiteSpace: "nowrap",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-          }}
+    return (
+        <AppBar
+            position="sticky"
+            sx={{
+                height: 48,
+                minHeight: 48,
+                justifyContent: "center",
+            }}
         >
-          𖦹 {GetTitle("Wait, I'm Ramping!")}
-        </Typography>
-      </Box>
-      <Divider />
-      <List>
-        {pages.map(({ label, path }) => (
-          <ListItem key={label} disablePadding>
-            <ListItemButton component="a" href={path} selected={location.pathname === path}>
-              <ListItemText primary={GetTitle(label)} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-        
-      </List>
-      <Box sx={{ flexGrow: 1 }} />
-      <Box sx={{ p: 2, display: "flex", alignItems: "center", gap: 1 }}>
-        {spec && <SpecDisplay spec={spec} />}
-        <IconButton onClick={toggleTheme} sx={{ color: "inherit", ml: "auto" }}>
-          {themeMode === "dark" ? <Brightness7 /> : <Brightness4 />}
-        </IconButton>
-      </Box>
-    </Box>
-  );
+            <Container maxWidth="xl">
+                <Toolbar
+                    disableGutters
+                    variant="dense"
+                    sx={{
+                        height: 48,
+                    }}
+                >
+                    {/* hamburger for mobile */}
+                    <Box sx={{ display: { xs: "flex", md: "none" }, alignItems: "center", mr: 1 }}>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                setDrawerOpen(true);
+                            }}
+                            sx={{ mr: 1 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    </Box>
 
-  return (
-    <AppBar
-      position="sticky"
-      sx={{
-        height: 48,
-        minHeight: 48,
-        justifyContent: "center",
-      }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar
-          disableGutters
-          variant="dense"
-          sx={{
-            height: 48,
-          }}
-        >
-          {/* hamburger for mobile */}
-          <Box sx={{ display: { xs: "flex", md: "none" }, alignItems: "center", mr: 1 }}>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              onClick={e => {
-                e.stopPropagation();
-                setDrawerOpen(true);
-              }}
-              sx={{ mr: 1 }}
+                    <Typography
+                        variant="h5"
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: "inline-flex",
+                            alignItems: "center",
+                            gap: 1,
+                            fontWeight: 700,
+                            color: location.pathname === "/" ? hoverColor : "inherit",
+                            textDecoration: "none",
+                            whiteSpace: "nowrap",
+                            fontSize: { xs: "1.1rem", md: "1.5rem" },
+                            flexGrow: { xs: 1, md: 0 },
+                            "&:hover": { color: hoverColor },
+                        }}
+                    >
+                        𖦹 {GetTitle("Wait, I'm Ramping!")}
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1 }} />
+
+                    {/* desktop navigation */}
+                    <Box sx={{ display: { xs: "none", md: "flex" }, alignItems: "center", gap: 1 }}>
+                        {pages.map(({ label, path }) => (
+                            <Button
+                                key={GetTitle(label)}
+                                component="a"
+                                href={path}
+                                sx={{
+                                    color: "inherit",
+                                    fontWeight: 400,
+                                    textTransform: "none",
+                                    position: "relative",
+                                    "&::before": {
+                                        content: '""',
+                                        position: "absolute",
+                                        bottom: 0,
+                                        left: 0,
+                                        right: 0,
+                                        height: 2,
+                                        bgcolor: "primary.main",
+                                        borderRadius: 1,
+                                        transform: location.pathname === path ? "scaleX(1)" : "scaleX(0)",
+                                        transition: "transform 0.3s ease",
+                                        zIndex: 2,
+                                    },
+                                    "&:hover::before": {
+                                        transform: "scaleX(1)",
+                                    },
+                                }}
+                            >
+                                {GetTitle(label)}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    {/* desktop settings box */}
+                    <Box 
+                        sx={{ 
+                            position: 'relative', 
+                            zIndex: 1, 
+                            color: 'white',
+                            backgroundColor: 'rgba(0, 0, 0, 0.3)',
+                            backdropFilter: 'blur(8px)',
+                            ml: 2,
+                            px: 1,
+                            py: 0.5,
+                            borderRadius: 3,
+                            boxShadow: '0 2px 8px rgba(0,0,0,0.15)',
+                            border: '1px solid rgba(255,255,255,0.1)',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            height: 'fit-content',
+                        }}
+                    >
+                        {spec && <SpecializationSelect selectedSpec={spec} onSpecChange={setSpec} short={true} />}
+                        <IconButton 
+                            onClick={toggleTheme}
+                            sx={{ color: "white" }}
+                        >
+                            {themeMode === "dark" ? <MoonIcon /> : <SunIcon />}
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </Container>
+
+            {/* mobile drawer */}
+            <Drawer
+                anchor="left"
+                open={drawerOpen}
+                onClose={handleDrawerToggle}
+                ModalProps={{ keepMounted: true }}
             >
-              <MenuIcon />
-            </IconButton>
-          </Box>
-
-          <Typography
-            variant="h5"
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: "inline-flex",
-              alignItems: "center",
-              gap: 1,
-              fontWeight: 700,
-              color: location.pathname === "/" ? hoverColor : "inherit",
-              textDecoration: "none",
-              whiteSpace: "nowrap",
-              fontSize: { xs: "1.1rem", md: "1.5rem" },
-              flexGrow: { xs: 1, md: 0 },
-              "&:hover": { color: hoverColor },
-            }}
-          >
-            𖦹 {GetTitle("Wait, I'm Ramping!")}
-          </Typography>
-
-          {/* desktop divider */}
-          <Divider
-            orientation="vertical"
-            flexItem
-            sx={{
-              display: { xs: "none", md: "block" },
-              mx: 0.5,
-              height: "100%",
-              alignSelf: "center",
-              borderColor: "rgba(255,255,255,0.2)"
-            }}
-          />
-
-          {/* desktop left side */}
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map(({ label, path }) => (
-              <Button
-                key={GetTitle(label)}
-                component="a"
-                href={path}
-                sx={{
-                  color: location.pathname === path ? hoverColor : "inherit",
-                  fontWeight: location.pathname === path ? 700 : 400,
-                  "&:hover": { color: hoverColor },
-                  textTransform: "none"
-                }}
-              >
-                {GetTitle(label)}
-              </Button>
-            ))}
-          </Box>
-          {/* desktop right side */}
-          <Box sx={{ display: { xs: "none", md: "flex" }, alignItems: "center", gap: 1 }}>
-            {spec && <SpecDisplay spec={spec} />}
-            <IconButton onClick={toggleTheme} sx={{ color: "white" }}>
-              {themeMode === "dark" ? <Brightness7 /> : <Brightness4 />}
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </Container>
-
-      {/* mobile drawer */}
-      <Drawer
-        anchor="left"
-        open={drawerOpen}
-        onClose={handleDrawerToggle}
-        ModalProps={{ keepMounted: true }}
-      >
-        {drawer}
-      </Drawer>
-    </AppBar>
-  );
+                {drawer}
+            </Drawer>
+        </AppBar>
+    );
 }
 
 export default ResponsiveAppBar;
