bc783a501c9ffe3f0526a4ce7328ed3ada2ace81
swirl <180882683+lolswirl@users.noreply.github.com>
2025-07-19 16:21:52 -0400

    add bugs path + reorder

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,23 @@ import * as React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
 
-import Home from "./pages/Home.tsx";
-import NotFound from "./pages/NotFound.tsx";
-import About from "./pages/About.tsx";
-import WhenDoIRamp from "./pages/WhenDoIRamp.tsx";
-import Timeline from "./pages/Timeline.tsx";
+import { useThemeContext } from './context/ThemeContext.tsx';
 import AppBar from "./components/AppBar/AppBar.tsx"
 import FooterBar from "./components/AppBar/FooterBar.tsx";
+
 import Tiling from "./components/Tiling/Tiling.tsx";
 import GridTiling from "./components/Tiling/GridTiling.tsx";
 import tileTransparent from "./assets/tile_transparent.png";
 import tileInverted from "./assets/tile_inverted.png";
 
-import { useThemeContext } from './context/ThemeContext.tsx';
 import { analysisPages } from "./pages/analysis/AnalysisPages.tsx";
+import Home from "./pages/Home.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import About from "./pages/About.tsx";
+import WhenDoIRamp from "./pages/WhenDoIRamp.tsx";
+import Timeline from "./pages/Timeline.tsx";
 import MissingIcons from "./components/MissingIcons/MissingIcons.tsx";
+import Bugs from "./pages/Bugs.tsx";
 
 function App() {
   const { themeMode } = useThemeContext();
@@ -47,6 +49,7 @@ function App() {
                 <Route path="/when-do-i-ramp" element={<WhenDoIRamp />} />
                 <Route path="/timeline" element={<Timeline />} />
                 <Route path="/missing" element={<MissingIcons />} />
+                <Route path="/bugs" element={<Bugs />} />
                 {analysisPages.map(({ path, element }) => (
                   <Route key={path} path={path} element={element} />
                 ))}
