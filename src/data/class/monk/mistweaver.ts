3ef83e74af42adbb1155619d9a55dc1a27706610
swirl <180882683+lolswirl@users.noreply.github.com>
2025-07-22 00:29:27 -0400

    init mw bugs

--- src/data/class/monk/mistweaver.ts
+++ src/data/class/monk/mistweaver.ts
@@ -1,15 +1,17 @@
+import { attachGetters } from '../attachGetters.ts';
+import { specialization } from '../class.ts';
 import MISTWEAVER_MONK_SPELLS from '../../spells/monk/mistweaver.ts';
 import MISTWEAVER_MONK_TALENTS from '../../talents/monk/mistweaver.ts';
 import MISTWEAVER_MONK_ROTATIONS from '../../rotations/monk/mistweaver.ts';
 import { MISTWEAVER_MONK_BUFFS } from '../../buffs/monk/mistweaver.ts';
-import { attachGetters } from '../attachGetters.ts';
-import { specialization } from '../class.ts';
+import MISTWEAVER_MONK_BUGS from '../../bugs/monk/mistweaver.ts';
 
 const MISTWEAVER_MONK = attachGetters({
   spells: MISTWEAVER_MONK_SPELLS,
   talents: MISTWEAVER_MONK_TALENTS,
   rotations: MISTWEAVER_MONK_ROTATIONS,
   buffs: MISTWEAVER_MONK_BUFFS,
+  bugs: MISTWEAVER_MONK_BUGS,
   icon: 'spell_monk_mistweaver_spec',
   name: 'Mistweaver',
   class: 'Monk',
