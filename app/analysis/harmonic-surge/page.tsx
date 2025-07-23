dd2ded487bc67f87eb59bbe1bae2239541dc39a7
swirl <180882683+lolswirl@users.noreply.github.com>
2025-07-23 14:06:37 -0400

    use client

--- app/analysis/harmonic-surge/page.tsx
+++ app/analysis/harmonic-surge/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
