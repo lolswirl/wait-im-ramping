8f24d6c7772a22c2f80781dcb9aebbe64e25af59
swirl <180882683+lolswirl@users.noreply.github.com>
2025-07-23 15:04:49 -0400

    delete changlog stuff

--- src/data/changelog/changelogs.tsx
+++
@@ -1,8 +0,0 @@
-import { change, date } from './changelog';
-import CONTRIBUTORS from '../contributor/contributors';
-
-const CHANGELOG = [
-  change(date(2025, 6, 20), "Added changelog.", CONTRIBUTORS.swirl),
-];
-
-export default CHANGELOG;
