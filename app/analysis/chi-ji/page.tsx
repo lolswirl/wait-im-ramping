54499c0b6497b6c3710debcd543d69cff95cdb9e
swirl <180882683+lolswirl@users.noreply.github.com>
2025-07-24 15:21:16 -0400

    reusable component for page metadata

--- app/analysis/chi-ji/page.tsx
+++ app/analysis/chi-ji/page.tsx
@@ -1,14 +1,10 @@
 import ChiJiPage from "./ChiJiPage";
-import { GetTitle } from "@util/stringManipulation";
 import { PageMetadata } from "@components/PageMetadata/PageMetadata";
 
-const title = GetTitle("Chi-Ji 'Simulation'");
-const description = GetTitle("Simulate the theoretical HPS outcome of rotations that affect Chi-Ji with tuning knobs for stats, enemy count, and talent choices");
+const title = "Chi-Ji 'Simulation'";
+const description = "Simulate the theoretical HPS outcome of rotations that affect Chi-Ji with tuning knobs for stats, enemy count, and talent choices";
 
-export const metadata = PageMetadata({
-  title,
-  description,
-});
+export const metadata = PageMetadata(title, description);
 
 export default function Page() {
   return <ChiJiPage title={title} description={description} />;
