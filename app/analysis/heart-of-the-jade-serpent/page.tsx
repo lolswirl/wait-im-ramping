bcba63722c883408a8750544dbb588f4d15fda48
swirl <180882683+lolswirl@users.noreply.github.com>
2026-02-16 12:02:39 -0500

    reword hotjs description again

--- app/analysis/heart-of-the-jade-serpent/page.tsx
+++ app/analysis/heart-of-the-jade-serpent/page.tsx
@@ -2,7 +2,7 @@ import HotJS from "./HotJS";
 import { PageMetadata } from "@components/PageMetadata/PageMetadata";
 
 const title = "Heart of the Jade Serpent";
-const description = "Calculate extra ability casts from Heart of the Jade Serpent's cooldown reduction";
+const description = "Analyze the effects of Heart of the Jade Serpent's increased cooldown recovery rate";
 export const metadata = PageMetadata(title, description);
 
 export default function Page() {
