4e23bcb1ebe3910f4369cef6d72e4026e0ab9bdd
swirl <180882683+lolswirl@users.noreply.github.com>
2026-05-21 12:07:39 -0400

    cleanup buff applications

--- src/data/specs/monk/mistweaver/buffs.ts
+++ src/data/specs/monk/mistweaver/buffs.ts
@@ -6,45 +6,34 @@ export const MISTWEAVER_MONK_BUFFS = (spellList: spell[]): spell[] => {
     let thunderFocusTeaUsed = false;
     let soothingMistUsed = false;
     let insideChiji = false;
+    let insideYulon = false;
 
-    let chijiStacks = 0;
     let totmStacks = 0;
 
     return spellList.map((spell) => {
+        // just guard around this, can probably make a thing later to not allow both being used but w/e
+        if (spell.id === SPELLS.CHI_JI.id) {
+            insideChiji = true;
+            insideYulon = false;
+            totmStacks = 4;
+        } else if (spell.id === SPELLS.YULON.id) {
+            insideYulon = true;
+            insideChiji = false;
+        }
+
         if (spell.id === SPELLS.THUNDER_FOCUS_TEA.id) {
             thunderFocusTeaUsed = true;
-        }
-        else if (spell.id === SPELLS.SOOTHING_MIST.id) {
+        } else if (spell.id === SPELLS.SOOTHING_MIST.id) {
             soothingMistUsed = true;
-        }
-        else if (spell.id === SPELLS.CHI_JI.id) {
-            insideChiji = true;
-        }
-        else if (insideChiji) {
-            if (spell.id === SPELLS.ENVELOPING_MIST.id) {
-                spell = {
-                    ...spell,
-                    castTime: GCD,
-                };
-            };
-        }
-        else if (thunderFocusTeaUsed) {
+        } else if (thunderFocusTeaUsed) {
             switch (spell.id) {
                 case SPELLS.ENVELOPING_MIST.id:
-                    spell = { 
-                        ...spell, 
-                        castTime: GCD 
-                    };
+                    spell = { ...spell, castTime: GCD };
                     thunderFocusTeaUsed = false;
                     break;
                 case SPELLS.RISING_SUN_KICK.id:
                 case SPELLS.RENEWING_MIST.id:
-                    spell = { 
-                        ...spell, 
-                        custom: {
-                            duration: 30,
-                        }
-                    };
+                    spell = { ...spell, custom: { duration: 30 } };
                     thunderFocusTeaUsed = false;
                     break;
                 case SPELLS.VIVIFY.id:
@@ -53,15 +42,11 @@ export const MISTWEAVER_MONK_BUFFS = (spellList: spell[]): spell[] => {
                 default:
                     break;
             }
-        }
-        else if (soothingMistUsed) {
+        } else if (soothingMistUsed) {
             switch (spell.id) {
                 case SPELLS.ENVELOPING_MIST.id:
                 case SPELLS.SHEILUNS_GIFT.id:
-                    spell = {
-                        ...spell,
-                        castTime: GCD
-                    };
+                    spell = { ...spell, castTime: GCD };
                     break;
                 case SPELLS.VIVIFY.id:
                 case SPELLS.LIFE_COCOON.id:
@@ -70,6 +55,23 @@ export const MISTWEAVER_MONK_BUFFS = (spellList: spell[]): spell[] => {
                     soothingMistUsed = false;
                     break;
             }
+        } else if (spell.id === SPELLS.BLACKOUT_KICK.id) {
+            totmStacks = 0;
+        } else if (spell.id === SPELLS.TIGER_PALM.id) {
+            // TODO: implement wotc here
+            totmStacks = Math.min(totmStacks + 1, 4);
+        }
+
+        // celestial mods happen after everything else
+        if (insideChiji && spell.id === SPELLS.ENVELOPING_MIST.id) {
+            spell = { ...spell, castTime: GCD };
+        } else if (insideYulon && spell.id === SPELLS.ENVELOPING_MIST.id) {
+            const base = spell.castTime ?? GCD;
+            spell = {
+                ...spell,
+                castTime: base * 0.7,
+                custom: { ...spell.custom, replaceGCD: 1 },
+            };
         }
 
         return spell;
