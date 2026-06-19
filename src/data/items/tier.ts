b8365bedcdde9a399b6b1e47f668d22bf520f76a
swirl <180882683+lolswirl@users.noreply.github.com>
2026-06-19 15:49:12 -0400

    use pc instead of -set

--- src/data/items/tier.ts
+++ src/data/items/tier.ts
@@ -2,32 +2,32 @@ import spell from '@data/spells/spell';
 
 const tier = {
     T34_LIGHTSMITH_2SET: {
-        name: "11.2 Lightsmith 2-Set",
+        name: "11.2 Lightsmith 2pc",
         id: 1236389,
         icon: 'inv_plate_raidpaladinethereal_d_01_glove',
     },
     T34_LIGHTSMITH_4SET: {
-        name: "11.2 Lightsmith 4-Set",
+        name: "11.2 Lightsmith 4pc",
         id: 1236390,
         icon: 'inv_plate_raidpaladinethereal_d_01_helm',
     },
     T35_MISTWEAVER_2SET: {
-        name: "12.0 Mistweaver 2-Set",
+        name: "12.0 Mistweaver 2pc",
         id: 1264840,
         icon: 'inv_shoulder_leather_raidmonkmidnight_d_01',
     },
     T35_MISTWEAVER_4SET: {
-        name: "12.0 Mistweaver 4-Set",
+        name: "12.0 Mistweaver 4pc",
         id: 1264841,
         icon: 'inv_helm_leather_raidmonkmidnight_d_01',
     },
     T36_MISTWEAVER_2SET: {
-        name: "12.1 Mistweaver 2-Set",
+        name: "12.1 Mistweaver 2pc",
         id: 1296619,
         icon: "inv_helm_leather_raidmonkulatek_d_01",
     },
     T36_MISTWEAVER_4SET: {
-        name: "12.1 Mistweaver 4-Set",
+        name: "12.1 Mistweaver 4pc",
         id: 1296620,
         icon: "inv_shoulder_leather_raidmonkulatek_d_01",
     }
