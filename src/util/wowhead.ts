74013c5c6a54e55f1dbdf94389516a8a163ce793
swirl <180882683+lolswirl@users.noreply.github.com>
2026-06-19 15:47:51 -0400

    add ptr link to wowhead

--- src/util/wowhead.ts
+++ src/util/wowhead.ts
@@ -1,9 +1,16 @@
+const isNonProd = (): boolean => {
+    const branch = process.env.NEXT_PUBLIC_CF_PAGES_BRANCH;
+    return !!branch && branch !== 'main' && branch !== 'master';
+};
+
 export const wowheadSpellUrl = (id: number, spellModifier?: number): string => {
-    return `https://www.wowhead.com/spell=${id}${spellModifier ? `?spellModifier=${spellModifier}` : ""}`;
+    const ptr = isNonProd() ? "ptr/" : "";
+    return `https://www.wowhead.com/${ptr}spell=${id}${spellModifier ? `?spellModifier=${spellModifier}` : ""}`;
 };
 
 export const wowheadItemUrl = (id: number): string => {
-    return `https://www.wowhead.com/item=${id}`;
+    const ptr = isNonProd() ? "ptr/" : "";
+    return `https://www.wowhead.com/${ptr}item=${id}`;
 };
 
 export const iconLocalUrl = (icon: string): string => {
