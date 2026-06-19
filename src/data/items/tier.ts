a96d762194b62310037d848cf78ae4ddb8bed404
swirl <180882683+lolswirl@users.noreply.github.com>
2026-06-19 15:48:19 -0400

    add 12.1 mw set + patch name in set

--- src/data/items/tier.ts
+++ src/data/items/tier.ts
@@ -2,25 +2,35 @@ import spell from '@data/spells/spell';
 
 const tier = {
     T34_LIGHTSMITH_2SET: {
-        name: "Lightsmith 2-Set",
+        name: "11.2 Lightsmith 2-Set",
         id: 1236389,
         icon: 'inv_plate_raidpaladinethereal_d_01_glove',
     },
     T34_LIGHTSMITH_4SET: {
-        name: "Lightsmith 4-Set",
+        name: "11.2 Lightsmith 4-Set",
         id: 1236390,
         icon: 'inv_plate_raidpaladinethereal_d_01_helm',
     },
     T35_MISTWEAVER_2SET: {
-        name: "Mistweaver 2-Set",
+        name: "12.0 Mistweaver 2-Set",
         id: 1264840,
         icon: 'inv_shoulder_leather_raidmonkmidnight_d_01',
     },
     T35_MISTWEAVER_4SET: {
-        name: "Mistweaver 4-Set",
+        name: "12.0 Mistweaver 4-Set",
         id: 1264841,
         icon: 'inv_helm_leather_raidmonkmidnight_d_01',
     },
+    T36_MISTWEAVER_2SET: {
+        name: "12.1 Mistweaver 2-Set",
+        id: 1296619,
+        icon: "inv_helm_leather_raidmonkulatek_d_01",
+    },
+    T36_MISTWEAVER_4SET: {
+        name: "12.1 Mistweaver 4-Set",
+        id: 1296620,
+        icon: "inv_shoulder_leather_raidmonkulatek_d_01",
+    }
 } satisfies Record<string, spell>;
 
 export default tier;
