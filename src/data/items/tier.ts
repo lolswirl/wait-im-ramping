32959484aacedc14f86043d61d11c1e6b17fe5f4
swirl <180882683+lolswirl@users.noreply.github.com>
2026-06-21 14:39:12 -0400

    standardize helm 4pc, shoulder 2pc

--- src/data/items/tier.ts
+++ src/data/items/tier.ts
@@ -1,10 +1,11 @@
 import spell from '@data/spells/spell';
 
+// helm for 4pc, shoulder for 2pc
 const tier = {
     T34_LIGHTSMITH_2SET: {
         name: "11.2 Lightsmith 2pc",
         id: 1236389,
-        icon: 'inv_plate_raidpaladinethereal_d_01_glove',
+        icon: 'inv_plate_raidpaladinethereal_d_01_shoulder',
     },
     T34_LIGHTSMITH_4SET: {
         name: "11.2 Lightsmith 4pc",
@@ -24,7 +25,7 @@ const tier = {
     T36_MISTWEAVER_2SET: {
         name: "12.1 Mistweaver 2pc",
         id: 1296619,
-        icon: "inv_helm_leather_raidmonkulatek_d_01",
+        icon: "inv_shoulder_leather_raidmonkulatek_d_01",
         custom: {
             rwkHealingIncrease: 1.0,
             rskDamageIncrease: 0.3,
@@ -33,7 +34,7 @@ const tier = {
     T36_MISTWEAVER_4SET: {
         name: "12.1 Mistweaver 4pc",
         id: 1296620,
-        icon: "inv_shoulder_leather_raidmonkulatek_d_01",
+        icon: "inv_helm_leather_raidmonkulatek_d_01",
         custom: {
             resetChance: 0.2,
         }
