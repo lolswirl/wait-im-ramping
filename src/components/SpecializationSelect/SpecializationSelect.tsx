56032a656113562753fcdc833811f42cbfc7bd08
swirl <180882683+lolswirl@users.noreply.github.com>
2026-07-09 10:19:05 -0400

    match glassmenu

--- src/components/SpecializationSelect/SpecializationSelect.tsx
+++ src/components/SpecializationSelect/SpecializationSelect.tsx
@@ -132,6 +132,8 @@ const SpecializationSelect: React.FC<SpecializationSelectProps> = ({
                 backdropFilter: "blur(8px)",
                 border: "1px solid rgba(255,255,255,0.1)",
                 boxShadow: "0 2px 8px rgba(0,0,0,0.15)",
+                maxHeight: 400,
+                borderRadius: 1,
                 backgroundImage: 'none',
               }
             }
